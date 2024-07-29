c5afc44ffd5e5d3d9aa53121ad149af9bc92f3b7
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-29 10:39:13 -0500

    added DB model and seeder

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 const port = process.env.PORT || 8000
+import mongoose from "mongoose"
 import products from "./data/products.js"
 
 const app = express()
@@ -19,6 +20,12 @@ app.get("/api/products/:id", (req, res, next) => {
     res.status(200).json(product)
 })
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+try {
+    await mongoose.connect(process.env.MONGO_URI)
+    app.listen(port, () => {
+        console.log(`server running on PORT ${port}`);
+    })
+} catch (error) {
+    console.log(error);
+    process.exit(1)
+}
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { Card } from 'react-bootstrap'
-import axios from 'axios';
 
 function Product({ product }) {
     // const [product, setProduct] = useState()
