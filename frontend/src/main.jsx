97156655539219a99f5efa7fd87c9e0b60dde6ac
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-08-03 22:03:59 -0500

    added login screen

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,6 +17,7 @@ import {
 import Home from './screens/Home'
 import ProductScreen from './screens/ProductScreen.jsx'
 import CartScreen from './screens/CartScreen.jsx'
+import LoginScreen from './screens/LoginScreen.jsx'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
             <Route index={true} path="/" element={<Home />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart" element={<CartScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
         </Route>
     )
 )
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { apiSlice } from "./slice/apiSlice"
 import cartSliceReducer from "./slice/cartSlice"
+import authSliceReducer from "./slice/authSlice"
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
-        cart: cartSliceReducer
+        cart: cartSliceReducer,
+        auth: authSliceReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true
